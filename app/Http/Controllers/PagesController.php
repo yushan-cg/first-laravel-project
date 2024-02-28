1b3c28bd498d1a0b30f4d810048f5c7b1a0677bf
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-28 14:29:55 +0800

    passing parameters throughout layouts using keyword 'compact'

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        $title = "Welcome to PHP Laravel Tutorials";
+        return view('pages.index', compact('title'));
     }
 
     public function about(){
