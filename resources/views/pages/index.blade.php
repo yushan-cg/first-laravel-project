e7be1b31da96eb2b3563ffe99c5a23c13e818764
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-28 14:15:15 +0800

    add pages controller

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'My Project Laravel')}}</title>
+
+    </head>
+
+    <body>
+        <h1>Welcome to bla bla bla</h1>
+        <p>desc...</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
 
 Route::get('/hello', function () {
     return '<h1>Hello World~</h1>';
 });
+
+/*use post when submitting via form
+Route::post('/hello', function () {
+    return '<h1>Hello World~</h1>';
+});*/
+
+/*Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/users/{id}', function ($id) {
+    return 'This is a user ' . $id;
+});
+*/
