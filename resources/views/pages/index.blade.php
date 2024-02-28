92e1f21aa212d7936d2b87db164560935320cbf0
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-28 14:24:23 +0800

    use @yield @section in layouts to simplify pages code

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'My Project Laravel')}}</title>
-
-    </head>
-
-    <body>
+@section('content')
         <h1>Welcome to bla bla bla</h1>
         <p>desc...</p>
-    </body>
-</html>
+@endsection
