78f0eba59c37f19a36496c188dfc966aa1261fd8
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-29 10:07:27 +0800

    Compile assets in Laravel

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use App\Http\Controllers\PagesController;
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
 Route::get('/service', [PagesController::class, 'service']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
