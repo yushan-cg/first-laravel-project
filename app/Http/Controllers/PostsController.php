92e6a4545041e3cf5a0972f64badb55cc8586850
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-29 13:36:39 +0800

    postscontroller and posts index for viewing info from database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
@@ -11,7 +12,9 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $title = "Posts";
+        $posts = Post::all();
+        return view('posts.index')->with(compact('title', 'posts'));
     }
 
     /**
