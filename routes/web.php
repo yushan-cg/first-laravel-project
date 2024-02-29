9787da541070d01c63adff7c0d03ee094a791535
imyushan <132455320+yushan-cg@users.noreply.github.com>
2024-02-29 11:31:10 +0800

    add routes related to PostsController

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/service', [PagesController::class, 'service']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+To generate the route code for adding resources using Laravel resourceful routing, 
+you can define a resource route in your routes/web.php file. 
+Assuming you have a PostsController and you want to create CRUD routes for posts, 
+you can use the Route::resource method.
+*/
+Route::resource('posts', App\Http\Controllers\PostsController::class);
